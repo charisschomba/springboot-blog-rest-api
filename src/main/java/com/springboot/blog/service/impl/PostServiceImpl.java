1c890720e24ff0258954787afdb50e5f24c5ccda
Tee Jay <charisschomba@gmail.com>
2023-02-24 03:40:24 -0500

    Implement create and get posts

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,56 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        // Convert DTO to Entity
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+
+        //  Convert entity to DTO
+        PostDto postDtoResponse = mapToDto(newPost);
+        return postDtoResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return  posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+    }
+
+    //  Convert entity to DTO
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+
+    // Convert DTO to Entity
+    private Post mapToEntity(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
+    }
+
+}
