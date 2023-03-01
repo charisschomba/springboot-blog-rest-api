ca2afb1b1fdcb66960ecf7bdec40a56b654325f2
chariss <chariss@debian-BULLSEYE-live-builder-AMD64>
2023-03-01 09:14:24 -0500

    Add comments to post response

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,10 +1,8 @@
 package com.springboot.blog.payload;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class PostDto {
@@ -12,4 +10,5 @@ public class PostDto {
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
